b603f36d3929ac13f661539dfdb6d90e0388b861
Ryan Le <ryanle88@gmail.com>
2018-12-23 22:50:53 -0600

    Updated LAMP script with github files

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Update CentOS with any patches
+yum update -y --execlude=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/ryanle88/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/ryanle88/vagrant/master/files/info.php
+
+service httpd restart
